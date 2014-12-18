1f61d8b8de0d44d893479f16707c104eaf0f45c7
Irina Belyaeva <ibelyaev@jcvi.org>
2014-12-18 03:17:22 -0500

    DBMS-35. Master Chado DW: Stock Centers Dataflow
    https://jira.araport.org/browse/DBMS-35
    
    1. Code for dbxref

--- stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
+++ stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
@@ -1,3 +1,10 @@
+-- validate if stock type exists in the dbxref table
+
+SELECT *
+  FROM chado.dbxref
+ WHERE accession = 'stock_type';
+
+
 -- validate if stock type exists in the Controlled Vocabularies table
 
 select * from CV where name = 'stock_type'

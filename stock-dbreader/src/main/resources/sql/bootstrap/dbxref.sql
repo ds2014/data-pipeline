43a1b24151f829186aed1423b544ba8cd54ebcc7
Irina Belyaeva <ibelyaev@jcvi.org>
2014-12-18 03:08:11 -0500

    DBMS-35. Master Chado DW: Stock Centers Dataflow
    https://jira.araport.org/browse/DBMS-35
    
    1. Code for dbxref

---
+++ stock-dbreader/src/main/resources/sql/bootstrap/dbxref.sql
@@ -0,0 +1,18 @@
+DO $$
+
+BEGIN
+WITH upd AS(
+UPDATE chado.dbxref dbx
+SET version = '', description = NULL
+WHERE accession='stock_type'
+RETURNING dbx.dbxref_id, dbx.db_id, dbx.accession, dbx.version, dbx.description
+)
+INSERT INTO chado.dbxref (db_id, accession, version, description)
+SELECT 1, 'stock_type', '', NULL
+WHERE NOT EXISTS(SELECT * FROM upd);
+
+END;
+
+$$;
+
+COMMIT;

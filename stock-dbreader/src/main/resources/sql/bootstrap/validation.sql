29086c6eaec4fe05219e344fae06b04d5b599fa6
Irina Belyaeva <ibelyaev@jcvi.org>
2014-12-18 14:55:40 -0500

    Date:   Thu Dec 18 14:31:55 2014 -0500
    DBMS-35. Master Chado DW: Stock Centers Dataflow
    
    https://jira.araport.org/browse/DBMS-35
    
    1. Code for stock_availaibility

--- stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
+++ stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
@@ -4,7 +4,14 @@ SELECT *
   FROM chado.dbxref
  WHERE accession = 'stock_type';
 
+-- validate if stock availability exists in the dbxref table
+
+SELECT *
+  FROM chado.dbxref
+ WHERE accession = 'stock_availability';
 
 -- validate if stock type exists in the Controlled Vocabularies table
 
-select * from CV where name = 'stock_type'
+SELECT *
+  FROM CV
+ WHERE name = 'stock_type'

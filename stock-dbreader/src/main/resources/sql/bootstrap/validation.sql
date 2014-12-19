2f299d300b356091a520a79f16ad227c10db24e0
Irina Belyaeva <ibelyaev@jcvi.org>
2014-12-19 15:31:10 -0500

    DBMS-35. Master Chado DW: Stock Centers Dataflow
    https://jira.araport.org/browse/DBMS-35
    
    1. Code to load dbxref related to the tair stock_types and code to load cvterms instances from the tair stock types.

--- stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
+++ stock-dbreader/src/main/resources/sql/bootstrap/validation.sql
@@ -19,3 +19,14 @@ SELECT *
  SELECT *
  FROM chado.CV
  WHERE name = 'stock_availability';
+
+ -- validate number of records in cvterm equal to the stock_types
+SELECT
+	*
+FROM
+	chado.cvterm cv JOIN dbxref dbx
+		ON
+		dbx.dbxref_id = cv.dbxref_id
+WHERE
+	dbx.db_id = staging.get_tair_db_id_by_name(
+	'TAIR')

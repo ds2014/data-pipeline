92e51dee7ea1ddffb657547234b51c80bf45b627
Irina Belyaeva <ibelyaev@jcvi.org>
2015-03-17 15:13:05 -0400

    stock_cvterm

--- src/main/resources/sql/schema/staging/init_staging_schema.sql
+++ src/main/resources/sql/schema/staging/init_staging_schema.sql
@@ -75,3 +75,79 @@ AS
    END;
    $$
    LANGUAGE plpgsql;
+   
+   
+   CREATE OR REPLACE FUNCTION staging.match_mutagen (text)
+   RETURNS varchar
+AS
+   $$
+   DECLARE
+      result   varchar;
+   BEGIN
+   		IF $1 IS NOT NULL THEN
+		IF $1 = 'T-DNA insertion'  THEN
+			result := 't_dna_insertion';
+		ELSEIF $1 = 'ethylmethane sulfonate' THEN
+			result := 'ethylmethane_sulfonate';
+		ELSEIF $1 = 'diepoxybutane' THEN
+			result := 'diepoxybutane';
+		ELSEIF $1 = 'nitrosomethyl urea' THEN
+			result := 'nitrosomethyl_urea';
+		ELSEIF $1 = 'HZE Ne' THEN
+			result := 'hze_ne';
+		ELSEIF $1 = 'dsRNA silencing' THEN
+			result := 'dsRNA_silencing';
+		ELSEIF $1 = 'X-rays' THEN
+			result := 'x_rays';
+		ELSEIF $1 = 'fast neutrons' THEN
+			result := 'fast_neutrons';
+		ELSEIF $1 = 'ethyl-nitrosourea' THEN
+			result := 'ethyl_nitrosourea';
+		ELSEIF $1 = 'tissue culture' THEN
+			result := 'tissue_culture';
+		ELSEIF $1 = 'spontaneous' THEN
+			result := 'spontaneous';
+		ELSEIF $1 = 'HZE U' THEN
+			result := 'hze_u';
+		ELSEIF $1 = 'nitrosomethyl biuret' THEN
+			result := 'nitrosomethyl_biuret';
+		ELSEIF $1 = 'Agrobacterium transformation' THEN
+			result := 'agrobacterium_transformation';
+		ELSEIF $1 = 'ionizing radiation' THEN
+			result := 'ionizing_radiation';	
+		ELSEIF $1 = '5-bromouracil' THEN
+			result := '5_bromouracil';	
+		ELSEIF $1 = 'sodium ascorbate' THEN
+			result := 'sodium_ascorbate';	
+		ELSEIF $1 = 'unknown' THEN
+			result := 'unknown';	
+		ELSEIF $1 = 'carbon ionization' THEN
+			result := 'carbonionization';
+		ELSEIF $1 = 'carbonionization' THEN
+			result := 'carbonionization';	
+		ELSEIF $1 = 'transposon insertion' THEN
+			result := 'transposon_insertion';
+		ELSEIF $1 = 'zinc finger nuclease' THEN
+			result := 'zinc_finger_nuclease';
+		ELSEIF $1 = 'HZE Kr' THEN
+			result := 'hze_kr';
+		ELSEIF $1 = 'HZE C' THEN
+			result := 'hze_c';
+		ELSEIF $1 = 'nitroguanidine' THEN
+			result := 'nitroguanidine';
+		ELSEIF $1 = 'gamma rays' THEN
+			result := 'gamma_rays';
+		ELSEIF $1 = '' THEN
+			result := 'unknown';
+		ELSE
+			result := $1;
+			
+		END IF;
+	ELSE
+		result := NULL;
+	END IF;
+       		       
+      RETURN result;
+   END;
+   $$
+   LANGUAGE plpgsql;
